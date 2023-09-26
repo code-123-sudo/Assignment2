c688d95cf10c8d2325d2878337b12184aa4708f9
Papa <papa@Papas-MacBook-Pro.local>
2023-09-26 21:34:59 +0530

    innitial fixes

--- src/components/Sidebar/Sidebar.js
+++ src/components/Sidebar/Sidebar.js
@@ -26,59 +26,24 @@ export const Sidebar = () => {
   return (
     <>
       <header className='header' id='header'>
-        <label className='switch'>
-          <input
-            type='checkbox'
-            onChange={handleChange}
-            name='mode'
-            checked={toggle}
-          />
-          <span className='slider round'></span>
-        </label>
-        <div className='header_toggle'>
-          <i className='bx bx-menu' id='header-toggle'></i>
-        </div>
-        <div className='header_img'>
-          <img src='https://i.imgur.com/hczKIze.jpg' alt='profile' />
-        </div>
       </header>
-      <div className='l-navbar' id='nav-bar'>
-        <nav className='nav'>
-          <div>
-            <NavLink className='nav_logo'>
-              <i className='bi bi-box-seam'></i>
-              <span className='nav_logo-name'>LG </span>
-            </NavLink>
-            <div className='nav_list'>
-              <NavLink href='#' className='nav_link'>
-                <i className='bi bi-bag-check' title='Items'></i>
-                <span className='nav_name'>Items</span>
-              </NavLink>
-              <NavLink href='#' className='nav_link'>
-                <i className='bi bi-person-vcard'></i>
-                <span className='nav_name'>Customers</span>
-              </NavLink>
-
-              <NavLink href='#' className='nav_link'>
-                <i className='bi bi-gear'></i>
-                <span className='nav_name'>Transactions</span>
-              </NavLink>
-              <NavLink href='#' className='nav_link'>
-                <i className='bi bi-person-gear'></i>
-                <span className='nav_name'>User Roles</span>
-              </NavLink>
-              <NavLink href='#' className='nav_link '>
-                <i className='bi bi-people'></i>
-                <span className='nav_name'>Users</span>
-              </NavLink>
-            </div>
-          </div>
-          <li onClick={onSignOut} className='nav_link' role='button'>
-            <i className='bi bi-box-arrow-right'></i>
-            <span className='nav_name'>Sign Out</span>
-          </li>
-        </nav>
-      </div>
+        <div className="topnav">
+          <a className="active" href="#home">Recco</a>
+          <a href="#news">Store</a>
+          <a href="#contact">Order</a>
+          <a href="#about">Analytics</a>
+          <a className="cart" href="#cart">Cart</a>
+          <a className="user" href="#cart">Hello James!</a>
+        </div>
+        <div className="nav2">
+          <p className="cart" >Order ></p>
+          <p className="user" >Order 32457ABC</p>
+        </div>
+        <div className="nav3">
+          <p className="orderId" >Order 32457ABC</p>
+          <div className="button1"><div className="button1p">Back</div></div>
+          <div className="button2"><div className="button2p">Approve Order</div></div>
+        </div>
     </>
   );
 };
